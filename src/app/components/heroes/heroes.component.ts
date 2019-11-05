1a2533adcbe77136436fc1e5fc6d5dd96a035f33
Arturo Lozano <alozano@domfinreal.corp>
2019-11-05 10:51:13 -0600

    added heroes array to service

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService } from '../../services/heroes.service';
+import { HeroesService, Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +7,15 @@ import { HeroesService } from '../../services/heroes.service';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor( private _heroesServie:HeroesService ) { }
+  heroes:Heroe[];
+
+  constructor( private _heroesService:HeroesService ) {
+    console.log("constructor!");
+  }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
   }
 
 }
